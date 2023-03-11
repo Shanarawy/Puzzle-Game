b56f02d5a3065f214c8ede27d165b02f109d95ce
Shanarawy <darkshan70@gmail.com>
2023-03-11 15:39:00 +0200

    Puzzle-Game

---
+++ game3/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button18 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(62, 52)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(161, 52)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 100)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button3.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(260, 52)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 100)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button4.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(359, 52)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 100)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button5.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(62, 151)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 100)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button6.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(161, 151)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 100)
+        Me.Button6.TabIndex = 0
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button7.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(260, 151)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 100)
+        Me.Button7.TabIndex = 0
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button8.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(359, 151)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 100)
+        Me.Button8.TabIndex = 0
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button9.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(62, 250)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 100)
+        Me.Button9.TabIndex = 0
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button10.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button10.Location = New System.Drawing.Point(161, 250)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(100, 100)
+        Me.Button10.TabIndex = 0
+        Me.Button10.Text = "10"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button11.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button11.Location = New System.Drawing.Point(260, 250)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(100, 100)
+        Me.Button11.TabIndex = 0
+        Me.Button11.Text = "11"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button12.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button12.Location = New System.Drawing.Point(359, 250)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(100, 100)
+        Me.Button12.TabIndex = 0
+        Me.Button12.Text = "12"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button13.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button13.Location = New System.Drawing.Point(62, 349)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(100, 100)
+        Me.Button13.TabIndex = 0
+        Me.Button13.Text = "13"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button14.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button14.Location = New System.Drawing.Point(161, 349)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(100, 100)
+        Me.Button14.TabIndex = 0
+        Me.Button14.Text = "14"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button15.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button15.Location = New System.Drawing.Point(260, 349)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(100, 100)
+        Me.Button15.TabIndex = 0
+        Me.Button15.Text = "15"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button16.Font = New System.Drawing.Font("Tahoma", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button16.Location = New System.Drawing.Point(359, 349)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(100, 100)
+        Me.Button16.TabIndex = 0
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button17.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button17.Location = New System.Drawing.Point(327, 460)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(165, 41)
+        Me.Button17.TabIndex = 1
+        Me.Button17.Text = "الصورة الأصلية"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(62, 52)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(397, 397)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label1.Location = New System.Drawing.Point(199, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(131, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "كون صورة نمر"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Button18
+        '
+        Me.Button18.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button18.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button18.Location = New System.Drawing.Point(39, 460)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(93, 41)
+        Me.Button18.TabIndex = 4
+        Me.Button18.Text = "إنهاء"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ClientSize = New System.Drawing.Size(522, 513)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Puzzle"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+End Class
---
+++ game3/Form1.vb
@@ -0,0 +1,225 @@
+﻿Public Class Form1
+
+    Dim count As Integer
+    Public Sub chechbut(ByRef btn1 As Button, ByRef btn2 As Button)
+        If btn2.Text = Nothing Then
+            btn2.Text = btn1.Text
+            btn1.Text = Nothing
+            btn2.BackgroundImage = btn1.BackgroundImage
+            btn1.BackgroundImage = Nothing
+        End If
+
+    End Sub
+    Public Sub endgame()
+        If Button1.Text = "1" And Button2.Text = "2" And Button3.Text = "3" And Button4.Text = "4" And Button5.Text = "5" And Button6.Text = "6" And Button7.Text = "7" And Button8.Text = "8" And Button9.Text = "9" And Button10.Text = "10" And Button11.Text = "11" And Button12.Text = "12" And Button13.Text = "13" And Button14.Text = "14" And Button15.Text = "15" Then
+            MsgBox(" لقد فزت  " & "  عدد الحركات" & count, MsgBoxStyle.Information, "تهانينا")
+        End If
+        count = count + 1
+
+    End Sub
+    '=======ترتيب الخانات عشوائياً=======
+    Sub Reset()
+        Dim a(15), i, j, RN As Integer
+        Dim flag As Boolean
+
+        flag = False
+        i = 1
+        a(j) = 1
+
+        Do While i <= 15
+            Randomize()
+            RN = CInt(Int((15 * Rnd()) + 1))
+
+            For j = 1 To i
+                If (a(j) = RN) Then
+                    flag = True
+                    Exit For
+                End If
+            Next
+
+            If flag = True Then
+                flag = False
+            Else
+                a(i) = RN
+                i = i + 1
+            End If
+        Loop
+
+        Button1.Text = a(1)
+        Button2.Text = a(2)
+        Button3.Text = a(3)
+        Button4.Text = a(4)
+        Button5.Text = a(5)
+
+        Button6.Text = a(6)
+        Button7.Text = a(7)
+        Button8.Text = a(8)
+        Button9.Text = a(9)
+        Button10.Text = a(10)
+
+        Button11.Text = a(11)
+        Button12.Text = a(12)
+        Button13.Text = a(13)
+        Button14.Text = a(14)
+        Button15.Text = a(15)
+        Button16.Text = ""
+        Button1.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(1))
+        Button2.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(2))
+        Button3.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(3))
+        Button4.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(4))
+        Button5.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(5))
+        Button6.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(6))
+        Button7.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(7))
+        Button8.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(8))
+        Button9.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(9))
+        Button10.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(10))
+        Button11.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(11))
+        Button12.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(12))
+        Button13.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(13))
+        Button14.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(14))
+        Button15.BackgroundImage = My.Resources.ResourceManager.GetObject("tiger_" & a(15))
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '====ترتيب الخانات عشوائيا عند بدأ اللعبة=====
+        Reset()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        chechbut(Button1, Button2)
+        chechbut(Button1, Button5)
+        endgame()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        chechbut(Button2, Button3)
+        chechbut(Button2, Button1)
+        chechbut(Button2, Button6)
+        endgame()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        chechbut(Button3, Button4)
+        chechbut(Button3, Button2)
+        chechbut(Button3, Button7)
+        endgame()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        chechbut(Button4, Button3)
+        chechbut(Button4, Button8)
+        endgame()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        chechbut(Button5, Button1)
+        chechbut(Button5, Button6)
+        chechbut(Button5, Button9)
+        endgame()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        chechbut(Button6, Button2)
+        chechbut(Button6, Button5)
+        chechbut(Button6, Button7)
+        chechbut(Button6, Button10)
+        endgame()
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        chechbut(Button7, Button3)
+        chechbut(Button7, Button8)
+        chechbut(Button7, Button6)
+        chechbut(Button7, Button11)
+        endgame()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        chechbut(Button8, Button4)
+        chechbut(Button8, Button7)
+        chechbut(Button8, Button12)
+        endgame()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        chechbut(Button9, Button5)
+        chechbut(Button9, Button13)
+        chechbut(Button9, Button10)
+        endgame()
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        chechbut(Button10, Button6)
+        chechbut(Button10, Button9)
+        chechbut(Button10, Button11)
+        chechbut(Button10, Button14)
+        endgame()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        chechbut(Button11, Button7)
+        chechbut(Button11, Button10)
+        chechbut(Button11, Button12)
+        chechbut(Button11, Button15)
+        endgame()
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        chechbut(Button12, Button8)
+        chechbut(Button12, Button16)
+        chechbut(Button12, Button11)
+        endgame()
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        chechbut(Button13, Button9)
+        chechbut(Button13, Button14)
+        endgame()
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        chechbut(Button14, Button10)
+        chechbut(Button14, Button15)
+        chechbut(Button14, Button13)
+        endgame()
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        chechbut(Button15, Button11)
+        chechbut(Button15, Button16)
+        chechbut(Button15, Button14)
+        endgame()
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        chechbut(Button16, Button12)
+        chechbut(Button16, Button15)
+        endgame()
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button17_Click_1(sender As Object, e As EventArgs) Handles Button17.Click
+
+    End Sub
+
+    Private Sub Button17_MouseDown(sender As Object, e As MouseEventArgs) Handles Button17.MouseDown
+        PictureBox1.Visible = True
+        PictureBox1.Image = My.Resources.tiger
+    End Sub
+
+    Private Sub Button17_MouseUp(sender As Object, e As MouseEventArgs) Handles Button17.MouseUp
+        PictureBox1.Visible = False
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        End
+    End Sub
+End Class
